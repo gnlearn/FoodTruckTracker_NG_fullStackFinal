a91f851270d6cd2e1ff45cd78036dec40cdd3563
Nico Gonnella <nico.gonnella@drake.edu>
2025-12-03 16:26:15 -0600

    complete database model

--- backend/models/Truck.js
+++ backend/models/Truck.js
@@ -1,7 +1,46 @@
 const mongoose = require('mongoose');
 
 const truckSchema = new mongoose.Schema({
-  
+  truckID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Truck',
+    required: true
+  },
+  truckName: {
+    type: String,
+    required: true, 
+    trim: true
+  },
+  cuisineType: {
+    type: String,
+    required: true, 
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true, 
+    trim: true
+  },
+  longitude: {
+    type: Number,
+    required: true
+  },
+  latitude: {
+    type: Number,
+    required: true
+  },
+  openTime: {
+    type: String,
+    required: true, 
+    trim: true
+  },
+  closeTime: {
+    type: String,
+    required: true, 
+    trim: true
+  }
+}, { 
+  timestamps: true 
 });
 
 const Truck = mongoose.model('Truck', truckSchema);
