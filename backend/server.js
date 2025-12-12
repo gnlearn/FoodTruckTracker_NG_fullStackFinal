3a7ba6bdff3ca5f723708728fb96bb58529ec964
Nico Gonnella <nico.gonnella@drake.edu>
2025-12-11 22:23:35 -0600

    removed trailing slash

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://foodtrucktrackercs195.netlify.app/'
+    'https://foodtrucktrackercs195.netlify.app'
   ],
   credentials: true
 }));
